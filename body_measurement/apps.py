4bb13985f5b9ef5e869c954ed6b2bf81fe676054
anton <antonchzhu@gmail.com>
2023-09-24 19:22:39 +0600

    init project

---
+++ body_measurement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BodyMeasurementConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "body_measurement"
---
+++ main.py
@@ -0,0 +1,20 @@
+from roboflow import Roboflow
+
+rf = Roboflow(api_key="bZybpgq1ZaXVZNo9RL7Y")
+project = rf.workspace().project("body-measurement-zmbgv")
+model = project.version(1).model
+
+# infer on a local image
+print(
+    model.predict(
+        "/Users/anton/Downloads/Image-Based-Body-Measurement-Detection-Challenge-main/Dataset/test/arm_circumference_img_25-0_jpg.rf.eb6ea95d2c858075c07feac975b495ee.jpg",
+        confidence=40,
+        overlap=30,
+    ).json()
+)
+
+# visualize your prediction
+# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
+
+# infer on an image hosted elsewhere
+# print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
